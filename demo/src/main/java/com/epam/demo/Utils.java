020db68775d7bf1d06c9c4e649e01577cc006390
Aleksey Pardaev <lpardaev607@gmail.com>
2024-05-07 23:17:24 +0300

    1

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        //magic happens here
+        for(String s : args) {
+           if(!com.epam.utils.StringUtils.isPositiveNumber(s)) return false;
+        }
+
+        return true;
     }
 }
